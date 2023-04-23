a0d2bf1fc7a504252714c7a98a95eed0a64c86ed
Maciej Rokiczan <maciej.rokiczan@icloud.com>
2023-04-23 16:32:04 +0200

    Initial Commit

---
+++ PhotoSelector/PhotoSelectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoSelectorApp.swift
+//  PhotoSelector
+//
+//  Created by Maciej Rokiczan on 23/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct PhotoSelectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
